11c96f266eb8d646b35fdb45b2176690ece37a63
Renato Vieira <rvbelo01@gmail.com>
2023-10-16 15:35:48 -0300

    CRUD de contas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Axios } from 'axios'
 import './App.css'
 
 const axiosClient = new Axios({
-  baseURL: 'https://viacep.com.br/ws/',
+  baseURL: 'http://localhost:3000',
   headers: {
     "Accept": 'application/json',
     "Content-Type": 'application/json'
@@ -11,32 +11,111 @@ const axiosClient = new Axios({
 })
 
 function App() {
-  const [ address, setAddress ] = useState('')
 
-  const handleSearch = (cep: string) => {
-    if(cep){
-      axiosClient.get(`${cep}/json/`)
-        .then(response => {
-          setAddress(response.data)
+  const [ contas, setContas ] = useState([])
+
+  const handleSubmit = () => {
+    const idEl = document.getElementById('id') as HTMLInputElement ;
+    const nameEl = document.getElementById('name') as HTMLInputElement ;
+    const descriptionEl = document.getElementById('description') as HTMLInputElement ;
+
+    const data = {
+      "id": idEl.value !== "" ? Number(idEl.value) : null,
+      "nome": nameEl.value,
+      "descricao": descriptionEl.value
+    }
+
+    if(data.id){
+      axiosClient.put(`/contas/${data.id}`, JSON.stringify(data))
+      .then(() => {
+        nameEl.value = ""
+        descriptionEl.value = ""
+        getAccounts()
+      })
+    } else {
+      axiosClient.post('/contas', JSON.stringify(data))
+        .then(() => {
+          nameEl.value = ""
+          descriptionEl.value = ""
+          getAccounts()
         })
     }
+
   }
 
   useEffect(()=>{
-    console.log(address)
-  }, [address])
+    getAccounts()
+  }, [])
+
+  const handleEdit = (account: any) => {
+    const idEl = document.getElementById('id') as HTMLInputElement ;
+    const nameEl = document.getElementById('name') as HTMLInputElement ;
+    const descriptionEl = document.getElementById('description') as HTMLInputElement ;
+
+    idEl.value = account.id
+    nameEl.value = account.nome
+    descriptionEl.value = account.descricao
+  }
+
+  useEffect(()=>{
+    getAccounts()
+  }, [])
+
+  const handleDelete = (id: number) => {
+    axiosClient.delete(`/contas/${id}`)
+      .then(getAccounts)
+  }
 
   return (
     <>
       <form>
-        <fieldset className='field-cep'>
-          <label htmlFor='cep'>Cep</label>
-          <input className='input-cep' type='text' id='cep' name='cep' onBlur={(e) => {handleSearch(e.target.value)}}/>
-        </fieldset>
-        <span>Resultado: {address}</span>
+
+        <input type='hidden' name="id" id='id' />
+
+        <label htmlFor='name'>Nome:</label>
+        <input id='name' name="name" />
+
+        <label htmlFor='description'>Descrição</label>
+        <input id='description' name="description" />
+        <button type='button' onClick={handleSubmit}>Enviar</button>
       </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Descrição</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            contas.map((c: any) => {
+              return (
+                <tr>
+                  <td>{c.id}</td>
+                  <td>{c.nome}</td>
+                  <td>{c.descricao}</td>
+                  <td>
+                    <button onClick={(_) => {handleEdit(c)}}>Editar</button>
+                    <button onClick={(_) => { handleDelete(c.id)}}>Excluir</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </>
   )
+
+  function getAccounts() {
+    axiosClient.get('/contas')
+      .then(response => {
+        setContas(JSON.parse(response.data))
+      })
+  }
 }
 
 export default App
