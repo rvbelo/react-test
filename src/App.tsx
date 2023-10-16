3d9c9a9762527013d9bc246d98c706afadb7ac1c
Renato Vieira <rvbelo01@gmail.com>
2023-10-16 14:35:00 -0300

    viacep searching result

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Axios } from 'axios'
 import './App.css'
+
+const axiosClient = new Axios({
+  baseURL: 'https://viacep.com.br/ws/',
+  headers: {
+    "Accept": 'application/json',
+    "Content-Type": 'application/json'
+  }
+})
 
 function App() {
   const [ address, setAddress ] = useState('')
 
   const handleSearch = (cep: string) => {
-    // http://viacep.com.br/ws/01001000/json/
+    if(cep){
+      axiosClient.get(`${cep}/json/`)
+        .then(response => {
+          setAddress(response.data)
+        })
+    }
   }
+
+  useEffect(()=>{
+    console.log(address)
+  }, [address])
 
   return (
     <>
       <form>
         <fieldset>
           <label htmlFor='cep'>Cep</label>
-          <input type='text' id='cep' name='cep' />
+          <input type='text' id='cep' name='cep' onBlur={(e) => {handleSearch(e.target.value)}}/>
         </fieldset>
         <span>Resultado: </span>
       </form>
